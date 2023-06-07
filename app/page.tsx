f4e2c4e4262113db5a3b6f1e725c3f75993edf09
brainvader <brainvader@gmail.com>
2023-06-07 13:37:07 +0000

    Add heading tag for page title

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-    </main>
+      <h1 className={styles.title}>飲み物提供表</h1>
+    </main >
   )
 }
