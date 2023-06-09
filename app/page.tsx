7e1aed592401880736ff0f4344c5633a641d0665
brainvader <brainvader@gmail.com>
2023-06-09 12:30:45 +0000

    refactor: Improve CSV input modal

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,51 @@
 
 import styles from './page.module.css'
 
-import { Button } from "@nextui-org/react";
-import React from 'react';
+import { Button, FormElement, Modal, Text, Textarea } from "@nextui-org/react";
+
+import { useState } from 'react';
 
 export default function Home() {
+  const [visible, setVisible] = useState(false);
+  const [csv, setCSV] = useState("名前, よみがな, コーヒー(Hミサ)")
+
+  const handler = () => setVisible(true);
+  const inputHandler = () => {
+    setVisible(false);
+    console.log(csv)
+  };
+
+  const inputDataHandler = (e: React.ChangeEvent<HTMLTextAreaElement | FormElement>) => {
+    setCSV(e.target.value)
+  }
+
   return (
     <main className={styles.main}>
       <div className={styles.inputCSV}>
-        <Button>Input CSV</Button>
+        <Button auto bordered onPress={handler}>Input CSV</Button>
+        <Modal
+          closeButton
+          aria-labelledby="csv input modal"
+          open={visible}
+          onClose={inputHandler}>
+          <Modal.Header>
+            <Text id="cvs-input" >
+              CSVデータを入力してください
+            </Text>
+          </Modal.Header>
+          <Modal.Body>
+            <Textarea
+              onChange={inputDataHandler}
+              initialValue={csv} />
+          </Modal.Body>
+          <Modal.Footer>
+            <Button auto color='primary' onPress={inputHandler}>
+              Input
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </div>
+
       <h1 className={styles.title}>飲み物提供表</h1>
       <div className={styles.container}>
         <table className={styles.column}>
