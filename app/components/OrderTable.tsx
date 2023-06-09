7fa84107179e8d65c40773aaba6afb869b83af4d
brainvader <brainvader@gmail.com>
2023-06-09 12:54:51 +0000

    refactor: move part of styles to a specific file

---
+++ app/components/OrderTable.tsx
@@ -0,0 +1,166 @@
+import styles from './OrderTable.module.css'
+
+export default function OrderTable() {
+    return (
+        < >
+            <h1 className={styles.title}>飲み物提供表</h1>
+            <div className={styles.container}>
+                <table className={styles.column}>
+                    <tbody>
+                        <tr>
+                            <td rowSpan={3}>あ</td>
+                            <td>あしべ</td>
+                            <td>コーヒー</td>
+                        </tr>
+                        <tr>
+                            <td>あべし</td>
+                            <td>コーヒー</td>
+                        </tr>
+                        <tr>
+                            <td>あべし</td>
+                            <td>コーヒー</td>
+                        </tr>
+                    </tbody>
+
+
+                    <tbody>
+                        <tr>
+                            <td rowSpan={3}>い</td>
+                            <td>なまえ</td>
+                            <td>コーヒー</td>
+                        </tr>
+                        <tr>
+                            <td>なまえ</td>
+                            <td>コーヒー</td>
+                        </tr>
+                        <tr>
+                            <td>なまえ</td>
+                            <td>コーヒー</td>
+                        </tr>
+                    </tbody>
+
+                    <tbody>
+                        <tr>
+                            <td rowSpan={3}>い</td>
+                            <td>なまえ</td>
+                            <td>紅茶</td>
+                        </tr>
+                        <tr>
+                            <td>なまえ</td>
+                            <td>紅茶</td>
+                        </tr>
+                        <tr>
+                            <td>なまえ</td>
+                            <td>紅茶</td>
+                        </tr>
+                    </tbody>
+
+                </table>
+
+                <table className={styles.column}>
+                    <tbody>
+                        <tr>
+                            <td rowSpan={3}>あ</td>
+                            <td>あしべ</td>
+                            <td>コーヒー</td>
+                        </tr>
+                        <tr>
+                            <td>あべし</td>
+                            <td>コーヒー</td>
+                        </tr>
+                        <tr>
+                            <td>あべし</td>
+                            <td>コーヒー</td>
+                        </tr>
+                    </tbody>
+
+
+                    <tbody>
+                        <tr>
+                            <td rowSpan={3}>い</td>
+                            <td>なまえ</td>
+                            <td>コーヒー</td>
+                        </tr>
+                        <tr>
+                            <td>なまえ</td>
+                            <td>コーヒー</td>
+                        </tr>
+                        <tr>
+                            <td>なまえ</td>
+                            <td>コーヒー</td>
+                        </tr>
+                    </tbody>
+
+                    <tbody>
+                        <tr>
+                            <td rowSpan={3}>い</td>
+                            <td>なまえ</td>
+                            <td>コーヒー</td>
+                        </tr>
+                        <tr>
+                            <td>なまえ</td>
+                            <td>コーヒー</td>
+                        </tr>
+                        <tr>
+                            <td>なまえ</td>
+                            <td>コーヒー</td>
+                        </tr>
+                    </tbody>
+
+                </table>
+
+                <table className={styles.column}>
+                    <tbody>
+                        <tr>
+                            <td rowSpan={3}>あ</td>
+                            <td>あしべ</td>
+                            <td>コーヒー</td>
+                        </tr>
+                        <tr>
+                            <td>あべし</td>
+                            <td>コーヒー</td>
+                        </tr>
+                        <tr>
+                            <td>あべし</td>
+                            <td>コーヒー</td>
+                        </tr>
+                    </tbody>
+
+
+                    <tbody>
+                        <tr>
+                            <td rowSpan={3}>い</td>
+                            <td>なまえ</td>
+                            <td>コーヒー</td>
+                        </tr>
+                        <tr>
+                            <td>なまえ</td>
+                            <td>コーヒー</td>
+                        </tr>
+                        <tr>
+                            <td>なまえ</td>
+                            <td>コーヒー</td>
+                        </tr>
+                    </tbody>
+
+                    <tbody>
+                        <tr>
+                            <td rowSpan={3}>い</td>
+                            <td>なまえ</td>
+                            <td>紅茶</td>
+                        </tr>
+                        <tr>
+                            <td>なまえ</td>
+                            <td>紅茶</td>
+                        </tr>
+                        <tr>
+                            <td>なまえ</td>
+                            <td>紅茶</td>
+                        </tr>
+                    </tbody>
+
+                </table>
+            </div>
+        </>
+    )
+}
